21845dce6e84c2cc9735b6f6ab4954c9018ea644
Vamsi Krishna <vamsitupakula474@gmail.com>
2023-02-06 18:41:59 +0530

    search bar customization done

--- src/components/SearchContainer.jsx
+++ src/components/SearchContainer.jsx
@@ -1,9 +1,15 @@
 import './css/SearchContainer.css'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function SearchContainer(props) {
     return ( 
         <div className={`search_bar_container ${props.theme}`} id='search-details'>
-
+            <div className="searchIcon">
+                <FontAwesomeIcon icon={faSearch} />
+            </div>
+            <input className="searchBar" type="text" placeholder='Enter username....'/>
+            <button className="searchBtn">Search</button>
         </div>
     );
 }
