5af85a93db7b6cd76d4433be746b3ede15adc458
Vamsi <vamsitupakula474@gmail.com>
2023-02-07 12:45:24 +0530

    octacat image added...

--- src/components/DetailsPane.jsx
+++ src/components/DetailsPane.jsx
@@ -3,7 +3,10 @@ import './css/DetailsPane.css';
 function DetailsPane(props) {
   return (
     <div className={`details_pane ${props.theme}`} id='user-details'>
-
+      <div className="githubImg">
+        <img src="https://avatars.githubusercontent.com/u/583231?v=4" alt="github-logo" className='githubLogo' width='140px'/>
+      </div>
+      <div className="details"></div>
     </div>
   );
 }
